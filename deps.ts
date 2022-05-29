3215b1c96b9c01e2c10e679ab42670f6342bc008
ninja03 <69106571+ninja03@users.noreply.github.com>
2022-05-29 23:23:01 +0900

    clinet-jsの変更の対応

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { default as ApiClient } from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.8/mod.ts";
+export { ApiClient } from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.9/mod.ts";
 export type {
   ActionPost,
   Game,
   MatchReq,
-} from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.8/mod.ts";
+} from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.9/mod.ts";
