9dbe0a1bd1d8fd76d11cb346edc2a41082bd8332
ninja03 <69106571+ninja03@users.noreply.github.com>
2022-05-29 23:41:46 +0900

    importをまとめました

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { default as ApiClient } from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.9/mod.ts";
-export type {
-  ActionPost,
-  Game,
-  MatchReq,
+export {
+  type ActionPost,
+  default as ApiClient,
+  type Game,
+  type MatchReq,
 } from "https://raw.githubusercontent.com/kakomimasu/client-js/v1.0.0-beta.9/mod.ts";
